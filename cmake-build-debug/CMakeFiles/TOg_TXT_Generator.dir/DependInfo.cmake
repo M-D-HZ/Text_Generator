d78154f4514d2e6c54aced2b0107689028e1002e
M-D-HZ <othman-hamzaoui@hotmail.com>
2022-05-21 20:58:37 +0200

    Cmake

---
+++ cmake-build-debug/CMakeFiles/TOg_TXT_Generator.dir/DependInfo.cmake
@@ -0,0 +1,36 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/mnt/c/Users/othma/OneDrive/Documents/School/UA_1BINF_SEM2/TAL/TOg/TOg_TXT_Generator/src/TOi/Anass/DFA.cpp" "/mnt/c/Users/othma/OneDrive/Documents/School/UA_1BINF_SEM2/TAL/TOg/TOg_TXT_Generator/cmake-build-debug/CMakeFiles/TOg_TXT_Generator.dir/src/TOi/Anass/DFA.cpp.o"
+  "/mnt/c/Users/othma/OneDrive/Documents/School/UA_1BINF_SEM2/TAL/TOg/TOg_TXT_Generator/src/TOi/Anass/NFA.cpp" "/mnt/c/Users/othma/OneDrive/Documents/School/UA_1BINF_SEM2/TAL/TOg/TOg_TXT_Generator/cmake-build-debug/CMakeFiles/TOg_TXT_Generator.dir/src/TOi/Anass/NFA.cpp.o"
+  "/mnt/c/Users/othma/OneDrive/Documents/School/UA_1BINF_SEM2/TAL/TOg/TOg_TXT_Generator/src/TOi/Anass/StateNFA.cpp" "/mnt/c/Users/othma/OneDrive/Documents/School/UA_1BINF_SEM2/TAL/TOg/TOg_TXT_Generator/cmake-build-debug/CMakeFiles/TOg_TXT_Generator.dir/src/TOi/Anass/StateNFA.cpp.o"
+  "/mnt/c/Users/othma/OneDrive/Documents/School/UA_1BINF_SEM2/TAL/TOg/TOg_TXT_Generator/src/TOi/Anass/TransitionDFA.cpp" "/mnt/c/Users/othma/OneDrive/Documents/School/UA_1BINF_SEM2/TAL/TOg/TOg_TXT_Generator/cmake-build-debug/CMakeFiles/TOg_TXT_Generator.dir/src/TOi/Anass/TransitionDFA.cpp.o"
+  "/mnt/c/Users/othma/OneDrive/Documents/School/UA_1BINF_SEM2/TAL/TOg/TOg_TXT_Generator/src/TOi/Anass/TransitionNFA.cpp" "/mnt/c/Users/othma/OneDrive/Documents/School/UA_1BINF_SEM2/TAL/TOg/TOg_TXT_Generator/cmake-build-debug/CMakeFiles/TOg_TXT_Generator.dir/src/TOi/Anass/TransitionNFA.cpp.o"
+  "/mnt/c/Users/othma/OneDrive/Documents/School/UA_1BINF_SEM2/TAL/TOg/TOg_TXT_Generator/src/TOi/Anass/main.cpp" "/mnt/c/Users/othma/OneDrive/Documents/School/UA_1BINF_SEM2/TAL/TOg/TOg_TXT_Generator/cmake-build-debug/CMakeFiles/TOg_TXT_Generator.dir/src/TOi/Anass/main.cpp.o"
+  "/mnt/c/Users/othma/OneDrive/Documents/School/UA_1BINF_SEM2/TAL/TOg/TOg_TXT_Generator/src/TOi/Chloe/DFA.cpp" "/mnt/c/Users/othma/OneDrive/Documents/School/UA_1BINF_SEM2/TAL/TOg/TOg_TXT_Generator/cmake-build-debug/CMakeFiles/TOg_TXT_Generator.dir/src/TOi/Chloe/DFA.cpp.o"
+  "/mnt/c/Users/othma/OneDrive/Documents/School/UA_1BINF_SEM2/TAL/TOg/TOg_TXT_Generator/src/TOi/Chloe/NFA.cpp" "/mnt/c/Users/othma/OneDrive/Documents/School/UA_1BINF_SEM2/TAL/TOg/TOg_TXT_Generator/cmake-build-debug/CMakeFiles/TOg_TXT_Generator.dir/src/TOi/Chloe/NFA.cpp.o"
+  "/mnt/c/Users/othma/OneDrive/Documents/School/UA_1BINF_SEM2/TAL/TOg/TOg_TXT_Generator/src/TOi/Chloe/RE.cpp" "/mnt/c/Users/othma/OneDrive/Documents/School/UA_1BINF_SEM2/TAL/TOg/TOg_TXT_Generator/cmake-build-debug/CMakeFiles/TOg_TXT_Generator.dir/src/TOi/Chloe/RE.cpp.o"
+  "/mnt/c/Users/othma/OneDrive/Documents/School/UA_1BINF_SEM2/TAL/TOg/TOg_TXT_Generator/src/TOi/Chloe/State.cpp" "/mnt/c/Users/othma/OneDrive/Documents/School/UA_1BINF_SEM2/TAL/TOg/TOg_TXT_Generator/cmake-build-debug/CMakeFiles/TOg_TXT_Generator.dir/src/TOi/Chloe/State.cpp.o"
+  "/mnt/c/Users/othma/OneDrive/Documents/School/UA_1BINF_SEM2/TAL/TOg/TOg_TXT_Generator/src/TOi/Yousra/Automata.cpp" "/mnt/c/Users/othma/OneDrive/Documents/School/UA_1BINF_SEM2/TAL/TOg/TOg_TXT_Generator/cmake-build-debug/CMakeFiles/TOg_TXT_Generator.dir/src/TOi/Yousra/Automata.cpp.o"
+  "/mnt/c/Users/othma/OneDrive/Documents/School/UA_1BINF_SEM2/TAL/TOg/TOg_TXT_Generator/src/TOi/Yousra/DFA.cpp" "/mnt/c/Users/othma/OneDrive/Documents/School/UA_1BINF_SEM2/TAL/TOg/TOg_TXT_Generator/cmake-build-debug/CMakeFiles/TOg_TXT_Generator.dir/src/TOi/Yousra/DFA.cpp.o"
+  "/mnt/c/Users/othma/OneDrive/Documents/School/UA_1BINF_SEM2/TAL/TOg/TOg_TXT_Generator/src/TOi/Yousra/NFA.cpp" "/mnt/c/Users/othma/OneDrive/Documents/School/UA_1BINF_SEM2/TAL/TOg/TOg_TXT_Generator/cmake-build-debug/CMakeFiles/TOg_TXT_Generator.dir/src/TOi/Yousra/NFA.cpp.o"
+  "/mnt/c/Users/othma/OneDrive/Documents/School/UA_1BINF_SEM2/TAL/TOg/TOg_TXT_Generator/src/main.cpp" "/mnt/c/Users/othma/OneDrive/Documents/School/UA_1BINF_SEM2/TAL/TOg/TOg_TXT_Generator/cmake-build-debug/CMakeFiles/TOg_TXT_Generator.dir/src/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../src/TOi/Anass"
+  "../src/TOi/Chloe"
+  "../src/TOi/Yousra"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/TOg_TXT_Generator.dir/cmake_clean.cmake
@@ -0,0 +1,23 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TOg_TXT_Generator.dir/src/TOi/Anass/DFA.cpp.o"
+  "CMakeFiles/TOg_TXT_Generator.dir/src/TOi/Anass/NFA.cpp.o"
+  "CMakeFiles/TOg_TXT_Generator.dir/src/TOi/Anass/StateNFA.cpp.o"
+  "CMakeFiles/TOg_TXT_Generator.dir/src/TOi/Anass/TransitionDFA.cpp.o"
+  "CMakeFiles/TOg_TXT_Generator.dir/src/TOi/Anass/TransitionNFA.cpp.o"
+  "CMakeFiles/TOg_TXT_Generator.dir/src/TOi/Anass/main.cpp.o"
+  "CMakeFiles/TOg_TXT_Generator.dir/src/TOi/Chloe/DFA.cpp.o"
+  "CMakeFiles/TOg_TXT_Generator.dir/src/TOi/Chloe/NFA.cpp.o"
+  "CMakeFiles/TOg_TXT_Generator.dir/src/TOi/Chloe/RE.cpp.o"
+  "CMakeFiles/TOg_TXT_Generator.dir/src/TOi/Chloe/State.cpp.o"
+  "CMakeFiles/TOg_TXT_Generator.dir/src/TOi/Yousra/Automata.cpp.o"
+  "CMakeFiles/TOg_TXT_Generator.dir/src/TOi/Yousra/DFA.cpp.o"
+  "CMakeFiles/TOg_TXT_Generator.dir/src/TOi/Yousra/NFA.cpp.o"
+  "CMakeFiles/TOg_TXT_Generator.dir/src/main.cpp.o"
+  "TOg_TXT_Generator"
+  "TOg_TXT_Generator.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TOg_TXT_Generator.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
