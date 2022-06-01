0f533bfb23163ebc79ff2f898f9b47d4c5afc382
M-D-HZ <othman-hamzaoui@hotmail.com>
2022-06-01 16:08:51 +0200

    Small fixes/changes

--- src/MarkovChain.cpp
+++ src/MarkovChain.cpp
@@ -12,7 +12,7 @@ using namespace std;
 
 bool isPunctuation(char c){
 //    vector<char> test = {'.',',','!',':','?','\n','(',')'};  Gebruik find voor ifstatement
-    if(c == '.' || c == ',' || c == '!' || c == ':' || c == '?' || c == '\n' || c == '(' || c == ')'){ // TODO: Dit moet korter en we moeten iets aan de haakjes doen :(
+    if(c == '.' || c == ',' || c == '!' || c == ':' || c == '?' || c == '\n'){ // TODO: Dit moet korter en we moeten iets aan de haakjes doen :(
         return true;
     }
     return false;
